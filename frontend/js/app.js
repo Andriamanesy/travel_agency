7424b780b934d0c201257f8b05bc5ec7315818cb
Andriamanesy <andriamanesybrian@gmail.com>
2026-08-05 20:31:05 +0300

    feat: connexion du frontend à l'api /api/hello

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("✅ Frontend JS chargé avec succès !");
+    const statusEl = document.getElementById('status');
+    const apiMessageEl = document.getElementById('api-message');
+
+    // Appel vers l'API Node.js à travers Nginx Proxy
+    fetch('/api/hello')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            statusEl.textContent = "Couche 1 & 2 connectées !";
+            apiMessageEl.textContent = data.message;
+        })
+        .catch(error => {
+            console.error("Erreur d'appel API :", error);
+            statusEl.textContent = "Impossible de joindre le Backend.";
+        });
 });
